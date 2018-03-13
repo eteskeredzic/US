bbe8718f69c0ca53d1e000cf3c9a6db642026d34
Edvin Teskeredzic <eteskeredz1@etf.unsa.ba>
2018-03-13 20:19:51 +0100

    Updated

--- LV1/5.cpp
+++ LV1/5.cpp
@@ -1,69 +1,16 @@
-/*
-
 #include "mbed.h"
 
 DigitalOut myled(LED1);
 
-int main() {
-    double  T = 0.1;
-    double  ukupno = 2*T;
-    int x = 0;
-    bool pozitivan = false;
-    // prvi run
-    while(1){
-        myled = 0;
-        wait(T + 0.1 * x);
-        myled = 1;
-        wait(T - 0.1 * x);
-        ++x;
-        if(x > 9) break;
-    }
-    x = 9;
-    while(1){
-        myled = 0;
-        wait(T + 0.1 * x);
-        myled = 1;
-        wait(T - 0.1 * x);
-        if(pozitivan) ++x;
-        else --x;
-        if(x > 18) {
-            pozitivan = false;
-            x ^= x;
-        }
-        else if(x < -18){
-        pozitivan = true;
-            x ^= x;
-        }
-    }
-
-
-
-
-    bool rast = true;
-
-    while(1) {
-        if(T>=ukupno)
-            T = 1.9 * ukupno;
-        if(T<=0)
-            T = 0.1 * ukupno;
-
-        myled = 1;
-        wait(ukupno-T+0.1);
-        myled = 0;
-        wait(T+0.1);
-
-        if(rast && T >= 1.9 * ukupno)
-            rast = false;
-        if (rast == false && T <= 0.1 * ukupno)
-            rast = true;
-
-        if(rast)
-            T += ukupno/60;
-        if(rast == false)
-            T -= ukupno/60;
-
-
+    int main() {
+    myled=1;
+    double T = 1,b=0.06 * T;
+    for(int i=T;1;i+=b)
+    {
+        myled=0;
+        wait(i);
+        myled=1;
+        wait(2*T-i);
+        if(i>=1.9*T || i<=0.1*T) b*=-1;
     }
 }
-
-*/
