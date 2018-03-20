16ea0aedac9c4a3d2a5dd8b4afed0b4cf9f830f5
kkarahodzi1 <37118273+kkarahodzi1@users.noreply.github.com>
2018-03-20 23:29:15 +0100

    Update 3b.cpp
    Popravljen raspored pinova, dodano čekanje da se pusti taster

--- LV2 v2.0/3b.cpp
+++ LV2 v2.0/3b.cpp
@@ -3,23 +3,12 @@
 DigitalOut enable(dp14);
 
 DigitalOut displej[] = {dp25, dp24, dp23};
-BusOut broj (dp26, dp27, dp5, dp6, dp1, dp2, dp4);
+BusOut broj (dp26, dp27, dp5, dp6, dp28, dp1, dp2);
 short int brojevi[] = {0, 64, 121, 36, 48, 25, 18, 2, 120, 0, 16, 8, 3, 70, 33, 9, 1};
 DigitalOut rows[] = {dp16, dp15, dp17, dp18};
 DigitalIn cols[] = {dp9, dp10, dp11, dp13};
 
 void prikaz(int brojac);
-
-int PopraviBrojac(int brojac){
-	int novi = 0;
-	novi += brojac % 10;
-	brojac /= 10;
-	novi += (brojac%10)*10;
-	brojac/=10;
-	novi += (brojac%10)*100;
-	return novi;
-}
-
 
 int main() {
     enable = 1;
@@ -34,6 +23,7 @@ int main() {
             brojac*=10;
             brojac+=i+1;
             prikaz(brojac);
+            while(cols[i]) prikaz(brojac);
             }
         rows[0] = 0;
         rows[1] = 1;
@@ -42,7 +32,7 @@ int main() {
             if(cols[i]){
             brojac*=10;
             brojac+=i+4;
-            prikaz(brojac);
+            prikaz(brojac); while(cols[i]) prikaz(brojac);
             }
         rows[1] = 0;
         rows[2] = 1;
@@ -51,14 +41,15 @@ int main() {
             if(cols[i]){
             brojac*=10;
             brojac+=i+7;
-            prikaz(brojac);
+            prikaz(brojac); while(cols[i]) prikaz(brojac);
             }
-        if(cols[3])
-            broj = 0;
-
+        if(cols[3]){
+            brojac = 0; prikaz(brojac); while(cols[3]) prikaz(brojac);
+}
         rows[2] = 0;
-		if(brojac > 999) brojac = PopraviBrojac(brojac);
+        if(brojac > 999) brojac = brojac % 1000;
         prikaz(brojac);
+        
     }
 }
 
@@ -66,17 +57,17 @@ void prikaz(int brojac){
     displej[0] = 1;
     displej[1] = 1;
     displej[2] = 1;
-	broj = brojevi[brojac%10+1];
+    broj = brojevi[brojac%10+1];
     displej[0] = 0;
     wait(0.001);
     brojac/=10;
     displej[0] = 1;
-	broj = brojevi[brojac%10+1];
+    broj = brojevi[brojac%10+1];
     displej[1] = 0;
     wait(0.001);
     brojac/=10;
     displej[1] = 1;
-    broj = brojevi[brojac%10+1];	
+    broj = brojevi[brojac%10+1];    
     displej[2] = 0;
     wait(0.001);
     displej[2] = 1;
