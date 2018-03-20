6b0a7e29ffd544e0a7138ce39c80accc2a6ad266
kkarahodzi1 <37118273+kkarahodzi1@users.noreply.github.com>
2018-03-20 23:26:50 +0100

    Update 1.cpp
    Uklonjena negacija ispred kolona

--- LV2 v2.0/1.cpp
+++ LV2 v2.0/1.cpp
@@ -12,19 +12,19 @@ int main() {
         rows[0] = 1; // pali prvi red
         wait(0.001); // cekaj da struja dodje
         for(int i = 0; i < 3; ++i) // vrti kroz kolone
-            leds[i] = !cols[i]; // ako je 1, upalis diodu
+            leds[i] = cols[i]; // ako je 1, upalis diodu
 
         rows[0] = 0; // gasi prvi red
         rows[1] = 1; // pali drugi red
         wait(0.001); // cekaj
         for(int i = 0; i < 3; ++i) // vrti kroz kolone
-            leds[i+3] = !cols[i]; // pali odgovarajuci LED
+            leds[i+3] = cols[i]; // pali odgovarajuci LED
 
         rows[1] = 0; // gasi drugi
         rows[2] = 1; // pali treci
         wait(0.001); // cekaj
         for(int i = 0; i < 3; ++i) // vrti kroz kolone
-            leds[i+6] = !cols[i]; // pali odgovarajuci LED
+            leds[i+6] = cols[i]; // pali odgovarajuci LED
 
         rows[2]  = 0; // gasi treci red
     }
